547c69ddeb9ae14909018f2f21353f09eeb19e1d
David Monarrez <davidmonarrez3@gmail.com>
2019-03-29 09:54:38 -0700

    trying to figure out how to get nav bar component to talk to the root and display nav-bar code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
@@ -10,7 +11,8 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
